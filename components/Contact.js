155baecdb6d4563cf09b691b3a689c182583d191
unknown <corintaogi2402@gmail.com>
2023-10-29 19:07:34 +0800

    react url router

---
+++ components/Contact.js
@@ -0,0 +1,9 @@
+function Contact() {
+    return(
+        <address>
+            You can find me here:<br />
+            Universitas Klabat<br />
+            Jl. Arnold Mononutu, Airmadidi Bawah, Kec. Airmadidi, <br />
+        </address>
+    );
+}
